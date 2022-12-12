ad0998ee04d64956e5e9cc3bf751728ed87e08dd
Ben Cravens <ben.conor.reese.cravens@gmail.com>
2022-12-12 17:11:17 +1300

    tests passing for human_readable

--- src/gptools/test_gp_util.py
+++ src/gptools/test_gp_util.py
@@ -3,10 +3,21 @@ from gptools.gp_util import human_readable
 
 class TestTikhonovRegularisation(unittest.TestCase):
 
-    def human_readable(self):
-        self.assertEqual(human_readable('vsub(vadd(f1,f1),f1)'), 'f1')
-        self.assertEqual(human_readable('vadd(f1,f2)'),'f1+f2')
-        self.assertEqual(human_readable('vsub(vmul(vadd(f1,f2),f3),f4)'),'(f1+f2)*f3 - f4')
+    def test_human_readable(self):
+        print("testing human_readable()")
+        self.assertEqual(
+            str(human_readable('vsub(vadd(f1,f1),f1)')).replace(" ", ""), 
+            'f1'
+        )
+        self.assertEqual(
+            str(human_readable('vadd(f1,f2)')).replace(" ", ""),
+            'f1+f2'
+        )
+        self.assertEqual(
+            str(human_readable('vsub(vmul(vadd(f1,f2),f3),f4)')).replace(" ", ""),
+            'f3*(f1+f2)-f4'
+        )
 
 if __name__ == '__main__':
+    print("running.")
     unittest.main()
