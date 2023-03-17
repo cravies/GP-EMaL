bb954c3c25ac585d72884409742f91ae0dd0ad92
Ben Cravens <ben.conor.reese.cravens@gmail.com>
2023-03-17 16:15:32 +1300

    Gather stats about trees from gpemal file. Doesnt work yet on gpmalmo

--- src/gptools/measure_trees.py
+++ src/gptools/measure_trees.py
@@ -43,11 +43,14 @@ def sigmoid(gamma):
     else:
         return 1 / (1 + math.exp(-gamma))
 
+def np_if(a, b, c):
+    return np.where(a < 0, b, c)
+
 #---- constants, i.e function set, cost set, pset object, operator cost dictionary ----
 #---- all this explained in paper -----
 
 NUM_FEATURES=4
-FS = "vadd,vsub,vmul,vdiv,max,min,relu,sigmoid"
+FS = "vadd,vsub,vmul,vdiv,max,min,relu,sigmoid,np_if"
 PSET = gp.PrimitiveSetTyped("MAIN", itertools.repeat(RealArray, NUM_FEATURES), ProxyArray, "f")
 
 if 'vadd' in FS:
@@ -68,30 +71,32 @@ if 'max' in FS:
     PSET.addPrimitive(np.maximum, [RealArray, RealArray], RealArray, name="max")
 if 'min' in FS:
     PSET.addPrimitive(np.minimum, [RealArray, RealArray], RealArray, name="min")
+if 'np_if' in FS:
+    PSET.addPrimitive(np_if, [RealArray, RealArray, RealArray], RealArray, name="np_if")
 
 #add possible features
 for i in range(2000):
     PSET.addTerminal(f"f{i}", RealArray)
 
-COSTS = "sum,sum,prod,prod,exp,exp,exp,exp,exp"
+COSTS = "sum,sum,prod,prod,exp,exp,exp,exp,exp,exp"
 COST_DICT = {k:v for k, v in zip(FS.split(','),COSTS.split(','))}
 
 #---- Complexity Measure ----
 
-def explore_tree_recursive(node_dict, subtree_root, indent, tree, labels, size=None):
+def tree_stats_recursive(stats_dict, subtree_root, indent, tree, labels, size=None):
     """
-    Traverses the tree and returns a dict which associates node indices
-    with operator, subtree size, and subtree asymmetry.
-    :param node_dict: The returned dictionary
+    Traverses the tree and returns a dict which stores statistics 
+    for various tree aspects like operator counts, 
+    total asymmetry, total node count
+    :param stats_dict: The returned dictionary
     :param subtree_root: The root node for the subtree. It is an index (int)
     :param indent: The indent string, Starts at ''
     :param tree: The overall DEAP tree object
     :param toolbox: The DEAP toolbox
     :param labels: graph labels 
-    :returns: node_dict, a dictionary that associates node indices with their subtree operator,
-    size, asymmetry, and children
+    :returns: stats_dict, a dictionary that collects tree stats
     
-    example node_dict for tree 'f1': {0: ['f1',1,0]}
+    example stats_dict for tree 'f1': {'asymmetry':1, 'exp_count':0, 'prod_count':0, 'linear_count':0, const_count:1, node_count:1}
     """
 
     #Lists for different types of operator
@@ -105,101 +110,78 @@ def explore_tree_recursive(node_dict, subtree_root, indent, tree, labels, size=N
         children.append([child_slice.start, child_slice.stop])
         i += 1
         idx = child_slice.stop
-
     node_op = tree[subtree_root].name
-    """
     if node_op[0]!='f':
         print(f"{indent}{node_op}(")
+        print(f"{indent}children: ",children)
     else:
         print(f"{indent}{node_op}")
-    """
-
+    asymmetry = 0
+    exp_count = 0
+    prod_count = 0
+    linear_count = 0
+    const_count = 0
+    node_count = 1 
+    
     # if we have a feature, just set constant complexity
     if node_op[0]=='f':
-        complexity = 2
-        size = 1
-        asymmetry=0
+        const_count = 1
     else:
-        # Else we recursively calculate complexity
-        complexity=0
         # also calculate asymmetry by breaking up subtree size into left and right subtree size
-        left_comp = 0
-        right_comp = 0
-        left_size = 0
-        right_size = 0
+        # recursively get counts from child subtree
         for i,child in enumerate(children):
+            #grab child stats dict
+            stats_dict_child = {'asymmetry':0, 'exp_count':0, 'prod_count':0, 'linear_count':0, 'node_count':0, 'const_count':0}
             child_index = child[0]
-            node_dict = explore_tree_recursive(node_dict, child_index, indent + ' |', tree, labels, size)
-            ##print(f"{indent}{node_dict}")
-            ##print(f"{indent}{node_dict[child_index]}")
-            child_complexity = node_dict[child_index][0]
-            child_size = node_dict[child_index][1]
-            if i==0:
-                left_comp += child_complexity
-                left_size += child_size
-            else:
-                right_comp += child_complexity
-                right_size += child_size
-        # add asymmetry penalty
-        size = left_size + right_size
-        asymmetry = abs(left_size - right_size)
-        complexity += 2**(asymmetry)-1
-        # calculate complexity based on node operation
-        cost = COST_DICT[node_op]
-        #print(f"op: {node_op}, cost: {cost}")
-        if cost.isdigit():
-            #we have a numerical cost
-            #print(f"{indent}const, {cost}")
-            complexity += int(cost) + left_comp + right_comp
-        elif cost=='sum':
-            #print(f"{indent}add, {left_comp}+{right_comp}")
-            complexity += left_comp + right_comp
-        elif cost=='prod':
-            #print(f"{indent}mul, {left_comp}*{right_comp}")
-            complexity += left_comp * right_comp
-        elif cost=='exp':
-            #print(f"{indent}exp, 2**({left_comp}+{right_comp})")
-            complexity = 2**(left_comp + right_comp)
-        else:
-            raise ValueError("Node cost error.")
-
-    #max out complexity at 1m 
-    if complexity>1e6:
-        complexity = float("inf")
-    
-    """
+            stats_dict_child = tree_stats_recursive(stats_dict_child, child_index, indent + ' |', tree, labels, size)
+            exp_count += stats_dict_child['exp_count']
+            prod_count += stats_dict_child['prod_count']
+            linear_count += stats_dict_child['linear_count']
+            const_count += stats_dict_child['const_count']
+            node_count += stats_dict_child['node_count']
+        # now we get count from our own node
+        if COST_DICT[node_op]=='exp':
+            exp_count += 1
+        elif COST_DICT[node_op]=='prod':
+            prod_count += 1
+        elif COST_DICT[node_op]=='sum':
+            linear_count += 1
+        elif COST_DICT[node_op]=='const':
+            const_count += 1
+    print(f"{indent}{stats_dict}")
     if node_op[0]!='f':
         print(f"{indent})")
-    print(f"{indent}size: {size} asymmetry:{asymmetry} complexity:{complexity}")
-    """
-    node_dict[subtree_root] = [complexity,size] 
-    # sort size dict by node index (i.e key)
-    node_dict = dict(sorted(node_dict.items()))
-    ##print(f"{indent}{tree[subtree_root].name} subtree size: {size}")
-    ##print("~"*30)
-    return node_dict
+    #store totals
+    stats_dict['asymmetry'] += asymmetry
+    stats_dict['exp_count'] += exp_count
+    stats_dict['prod_count'] += prod_count
+    stats_dict['linear_count'] += linear_count
+    stats_dict['node_count'] += node_count
+    stats_dict['const_count'] += const_count
+    return stats_dict
 
-
-
-def load_trees(path: str) -> NoReturn:
+def load_trees(path: str, oldnew='new') -> NoReturn:
     """ Load a set of .tree files from a directory
     and measure all of them
     Arguments:
     path: relative path (from this file) to the folder to walk
+    oldnew: are we reading from old files? (GPMaLMO) 
+    or new files (GPEMaL) cause they have different formats.
     """
     files = os.listdir(path)
     for file in files:
         if file.endswith('.tree'):
-            measure_tree(f"{path}/{file}")
+            if oldnew=='new':
+                measure_tree_new(f"{path}/{file}")
+            else:
+                measure_tree_old(f"{path}/{file}")
 
-def measure_tree(path: str) -> NoReturn:
+def measure_tree_new(path: str) -> NoReturn:
     """
     Measures the complexity of a specific tree file
     Arguments:
     path: relative path 
     """
-    count=0
-    complexity=0
     f = open(path)
     print("~"*30 + path + "~"*30)
     lines = f.readlines()
@@ -207,11 +189,23 @@ def measure_tree(path: str) -> NoReturn:
         if ' | ' in line:
             line = line.split(' | ')
             tree = line[1]
-            complexity += main(f"{count}_out",tree,FS)
-            count += 1
-    print("~"*30)
-    print("Total individual complexity: ",complexity)
-    print("~"*30)
+            file_line, stats_dict = main(tree,FS)
+            print(file_line)
+
+def measure_tree_old(path: str) -> NoReturn:
+    """
+    Measures the complexity of a specific tree file
+    in old GPMaLMO format.
+    Arguments:
+    path: relative path 
+    """
+    f = open(path)
+    print("~"*30 + path + "~"*30)
+    lines = f.readlines()
+    for line in lines:
+        tree = line.replace('\n','')
+        file_line, stats_dict = main(tree,FS)
+        print(file_line)
 
 def scaling_term(tree,mu=0.8,max_depth=8):
     """
@@ -228,28 +222,25 @@ def scaling_term(tree,mu=0.8,max_depth=8):
     else:
         return 2*alpha
 
-def main(name: str, tree: str, fs: str) -> NoReturn:
+def main(tree: str, fs: str) -> NoReturn:
     """
     Calculate the complexity of a given GP tree individual.
     Arguments:
-    name - name for the .png tree plot we are making
     tree -- the tree (a str) read from the .tree output file
     fs - the functional set for the GP algorithm (a str)
     returns: total_complexity - copmlexity of tree (float) 
     """ 
-
     # convert string to gp PrimitiveTree using from_string @classmethod
     # need to do this so we can calculate complexity 
+    tree_orig = tree
     tree = gp.PrimitiveTree.from_string(tree, PSET)
-
     nodes, edges, labels = gp.graph(tree)
-    node_dict = explore_tree_recursive({}, 0, '', tree, labels)
-    total_complexity=node_dict[0][0]
-    total_complexity*=scaling_term(tree)
-    
-    print(f"Tree: | {str(tree)} | complexity: {total_complexity}")
-    return total_complexity
+    stats_dict = {'asymmetry':0, 'exp_count':0, 'prod_count':0, 'linear_count':0, 'const_count':0, 'node_count':0}
+    stats_dict = tree_stats_recursive(stats_dict, 0, '', tree, labels)
+    treestr = str(tree_orig).replace("'","")
+    mystr=f"tree: | {treestr} | stats: {stats_dict}"
+    return mystr, stats_dict
 
 if __name__=="__main__":
-    measure_tree('./COIL20_pt2/COIL20_run_20/COIL20-0.03500168837096679-70.0.tree')
+    #measure_tree_old('winegpmalmo/1/wine-0.02216947891790294-7.0.tree')
     load_trees('./COIL20_pt2/COIL20_run_1')
