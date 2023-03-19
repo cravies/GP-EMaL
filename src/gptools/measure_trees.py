9c0fb5df1646ccbae184a81ff1a20ba0f64a20ed
Ben Cravens <ben.conor.reese.cravens@gmail.com>
2023-03-20 09:04:41 +1300

    formatting, add folder recursion, add csv output

--- src/gptools/measure_trees.py
+++ src/gptools/measure_trees.py
@@ -7,6 +7,7 @@ from typing import NoReturn
 import os
 from os import path
 import re
+import csv
 
 FS = "vadd,vsub,vmul,vdiv,max,min,relu,sigmoid,np_if"
 COSTS = "sum,sum,prod,prod,exp,exp,exp,exp,exp,exp"
@@ -25,7 +26,7 @@ def tree_stats_iterative(tree):
     stats_dict['unique_feats']=uniq_len
     return uniq, stats_dict
 
-def load_trees(path: str, oldnew='new') -> NoReturn:
+def run_folder(path, oldnew='new'):
     """ Load a set of .tree files from a directory
     and measure all of them
     Arguments:
@@ -35,13 +36,18 @@ def load_trees(path: str, oldnew='new') -> NoReturn:
     """
     files = os.listdir(path)
     for file in files:
+        fstr=f"{path}/{file}"
         if file.endswith('.tree'):
             if oldnew=='new':
-                measure_tree_new(f"{path}/{file}")
+                measure_tree_new(fstr)
             else:
-                measure_tree_old(f"{path}/{file}")
+                measure_tree_old(fstr)
+        # if a directory, recurse
+        if os.path.isdir(fstr):
+            print("recursing down: ",fstr)
+            run_folder(fstr,oldnew)
 
-def measure_tree_new(path: str) -> NoReturn:
+def measure_tree_new(path):
     """
     Measures the complexity of a specific tree file
     Arguments:
@@ -68,8 +74,13 @@ def measure_tree_new(path: str) -> NoReturn:
     print("~"*30)
     print("Individual total stats: ",str(stats_dict))
     print("~"*30)
+    # add path to stats dict
+    stats_dict['filename']=path
+    stats_dict['algo']="OLD"
+    # write results to csv file
+    json_write(stats_dict)
 
-def measure_tree_old(path: str) -> NoReturn:
+def measure_tree_old(path):
     """
     Measures the complexity of a specific tree file
     in old GPMaLMO format.
@@ -94,8 +105,13 @@ def measure_tree_old(path: str) -> NoReturn:
     print("~"*30)
     print("Individual total stats: ",str(stats_dict))
     print("~"*30)
+    # add path to stats dict
+    stats_dict['filename']=path
+    stats_dict['algo']="NEW"
+    # write results to csv file
+    json_write(stats_dict)
 
-def count_unique_features(tree: str):
+def count_unique_features(tree):
     """
     counts the number of unique features in a tree str
     """
@@ -103,7 +119,7 @@ def count_unique_features(tree: str):
     tree_arr = [i for i in tree_arr if 'i' not in i and 'f' in i]
     return tree_arr
 
-def main(tree: str, fs: str):
+def main(tree, fs):
     """
     Calculate the complexity of a given GP tree individual.
     Arguments:
@@ -119,9 +135,36 @@ def main(tree: str, fs: str):
     mystr+="~"*30 + "\n" + "stats: \n" + str(stats_dict) + "\n" + "~"*30 + "\n"
     return uniq, mystr, stats_dict
 
+def json_write(data_dict, filename='tree_stats.csv'):
+    """
+    Takes a output dict, and writes it to our 
+    output .csv file
+    Arguments:
+    output_dict: the dictionary whose values we want to write to our CSV file
+    output_filename: our output csv filename
+    columns:
+    exp,prod,sum,const,nodes,unique_feats,filename,algo
+    """
+    # check if file already exists. If it doesn't, we
+    # need to write our columns first
+    if os.path.exists(filename):
+        # file exists, append data to it
+        with open(filename, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(data_dict.values())
+    else:
+        # file does not exist, write columns and then data
+        with open(filename, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(data_dict.keys())
+            writer.writerow(data_dict.values())
+
 if __name__=="__main__":
     # EXAMPLES:
     # run on a specific tree file
-    measure_tree_old('winegpmalmo/1/wine-0.02216947891790294-7.0.tree')
+    #measure_tree_old('winegpmalmo/1/wine-0.02216947891790294-7.0.tree')
     # run on every tree file in a directory
-    #load_trees('./COIL20_pt2/COIL20_run_1')
+    run_folder('COIL20_pt2/')
+    #run_folder('winegpmalmo','old')
+    # recurse on COIL20_pt2 folder
+    #run_folder('./COIL20_pt2/')
